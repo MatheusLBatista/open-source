acab2abd2851051a653b0ee8069ea9c8ae2f417d
MatheusLBatista <matheusifro2020@gmail.com>
2025-08-28 22:08:17 -0400

    adding header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import "./style.css"
+
+export default function Header() {
+
+    return(
+        <header>
+            <div className="info-text">
+                <h1>Open Source Projects</h1>
+            </div>
+        </header>
+    )
+}
